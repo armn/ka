5357dace5180693dc4ea3a198197e718368ae612
Armands Leimanis <armands@gmail.com>
2019-05-06 00:11:21 +0300

    updated blogroll

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -24,7 +24,7 @@ class BlogRoll extends React.Component {
                       <PreviewCompatibleImage
                         imageInfo={{
                           image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${
+                          alt: `attēls rakstam ${
                             post.title
                           }`,
                         }}
